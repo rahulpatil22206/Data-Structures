9cb9487bf2727834d6b7bf1e088de34df4680f69
rahulpatil22206 <53813613+rahulpatil22206@users.noreply.github.com>
2019-08-12 11:23:10 +0530

    Add files via upload

---
+++ hello world/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c;
+    printf("enter the values of a and b");
+    scanf("%d %d",&a, &b);
+    c=a+b;
+    printf("the value of c is %d",c);
+    return 0;
+    }
